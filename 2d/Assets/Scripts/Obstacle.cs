616c90142a99fbb8866cdf42063f9158f41f5298
thebigh2nr03 <66183008+thebigh2nr03@users.noreply.github.com>
2022-09-21 13:08:06 -0400

    script changes

--- 2d/Assets/Scripts/Obstacle.cs
+++ 2d/Assets/Scripts/Obstacle.cs
@@ -2,21 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SharkMove : MonoBehaviour
+public class Obstacle : MonoBehaviour
 {
     public float speed = 1.5f;
     // Might be good to have varying speeds based on the sprite
     public float timeDestroy = 50.0f;
+    public float destroyPosition = -10;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Destroy(this.gameObject, timeDestroy);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.right * speed * -1 * Time.deltaTime);
+
+        if (transform.position.x <= destroyPosition){
+             Destroy(gameObject);
+        }
     }
 }
--- 2d/Assets/Scripts/SpawnEnemy.cs
+++ 2d/Assets/Scripts/SpawnEnemy.cs
@@ -10,11 +10,8 @@ public class SpawnEnemy : MonoBehaviour
 
     public float topLocation;
     public float bottomLocation;
-    public GameObject PinkJellyfish;
-    public GameObject GreenJellyfish;
-    public GameObject Starfish;
-    public GameObject BigShark;
-    public GameObject Treasure;
+    public GameObject[] SpawnableObjects;
+
 
     float currSpawnTime;
     // five seconds at varying intervals given that there are multiple types of enemies
@@ -22,7 +19,7 @@ public class SpawnEnemy : MonoBehaviour
     void Start()
     {
         currSpawnTime = maxSpawnTime;
-        Random.InitState(randomSeed);
+        //Random.InitState(randomSeed);
     }
 
     // Update is called once per frame
@@ -35,28 +32,10 @@ public class SpawnEnemy : MonoBehaviour
         currSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
         Vector3 location = Vector3.zero;
         location.x = 10;
-        bottomLocation = -10;
-        topLocation = 10;
         location.y = Random.Range(bottomLocation, topLocation);
         // pick a random enemy and a random y-coordinate
-        GameObject newObj;
-        switch(Random.Range(1, 5)) {
-            case 1:
-                newObj = Instantiate(PinkJellyfish, location, Quaternion.identity);
-                break;
-            case 2: 
-                newObj = Instantiate(BigShark, location, Quaternion.identity);
-                break;
-            case 3:
-                newObj = Instantiate(Starfish, location, Quaternion.identity);
-                break;
-            case 4:
-                newObj = Instantiate(GreenJellyfish, location, Quaternion.identity);
-                break;
-            case 5:
-                newObj = Instantiate(Treasure, location, Quaternion.identity);
-                break;
-        }
+        //GameObject newObj;
+        Instantiate(SpawnableObjects[Random.Range(0, 4)], new Vector2(10, Random.Range(bottomLocation, topLocation)), Quaternion.identity);
         
     }
 }
